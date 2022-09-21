a4f73fe0fbd25767cc0cb539232e32d9c981af99
sam2348 <110401519+sam2348@users.noreply.github.com>
2022-09-22 00:05:13 +0530

    Add files via upload

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,48 +1,61 @@
 import React, { useState,useEffect } from 'react';
 import { NavLink,useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { auth } from './Firebase';
 
 const Login = () => {
     const navigate = useNavigate()
-    const[errorMsg,setErrorMsg]=useState("")
+    const[emailErrorMsg,setEmailErrorMsg]=useState('')
+    const[passwordErrorMsg,setPasswordErrorMsg]=useState('')
+    const[errorMsg,setErrorMsg]=useState('')
     const[ragisterData,setragisterData]=useState({
         emailaddress:"",
         password:"",
     })
     const LoginHandler =()=>{
-        if(!ragisterData.emailaddress || !ragisterData.password){
-            setErrorMsg("fill all the fields");
+        
+        var regExx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+        var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!regEx.test(ragisterData.emailaddress)) {
+            setEmailErrorMsg("enter vaild email address");
+            return;
+        }else if(!regExx.test(ragisterData.password)){
+            setPasswordErrorMsg("enter vaild password");
             return;
         }
-        setErrorMsg(" ");
+        setEmailErrorMsg(" ");
+        setPasswordErrorMsg(" ");
         signInWithEmailAndPassword(auth,ragisterData.emailaddress,ragisterData.password)
         .then(()=>{
           localStorage.setItem('login',true)
           navigate('/')
         }
         ).catch((err)=> {
-            setErrorMsg(err.message);
+            toast.error(err.message);
+            
         });
+        
     };
     const emailInputHandler=(event)=>{
-        var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!regEx.test(event.target.value)) {
-            setErrorMsg("enter vaild email address");
+        // var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        // if (!regEx.test(event.target.value)) {
+        //     setErrorMsg("enter vaild email address");
             
-        }
+        // }
         setragisterData((prev)=>({
             ...prev,
             [event.target.name]: event.target.value,
         })) 
     }
     const passwordInputHandler=(event)=>{
-        var regExx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-        if (!regExx.test(event.target.value)) {
-            setErrorMsg("enter vaild password");
+        // var regExx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+        // if (!regExx.test(event.target.value)) {
+        //     setErrorMsg("enter vaild password");
             
-        }
+        // }
         setragisterData((prev)=>({
             ...prev,
             [event.target.name]: event.target.value,
@@ -67,19 +80,18 @@ const Login = () => {
                                   <img src="img/payoman-logo1.png" style={{ width: 300 }} />
                               </div>
                                 <h5 className="form-heading mb-4 p-2 text-center">Login to payoman</h5>
-                                 <div className='errorMsg'>
-                                 <div>{errorMsg}</div >
-                                 </div>
-                              <div className="input-group mb-3">
+                              <div className="input-group mb-1">
                                   <input type="text" 
                                   name='emailaddress' onChange={emailInputHandler} 
                                   className="form-control" id="exampleInputNumber1" placeholder="Enter phone number" />
                               </div>
-                              <div className="input-group mb-3">
+                              <span className='errorMsg '>{emailErrorMsg}</span >
+                              <div className="input-group mb-1 mt-2">
                                   <input type="password" 
                                   name='password' onChange={passwordInputHandler} 
                                   className="form-control" id="exampleInputPassword1" placeholder="Enter password" />
                               </div>
+                              <span className='errorMsg '>{passwordErrorMsg}</span >
                               <div className="input-group ">
                                   <div className="col-sm text-end">
                                       <NavLink to="/forgetpassword">Forgot Password?</NavLink>
@@ -89,6 +101,7 @@ const Login = () => {
                               <button type="button" className="btn btn-primary" 
                                onClick={LoginHandler} 
                               > Login&nbsp; <i className="fa-solid fa-right-to-bracket" /></button>
+                              < ToastContainer />
                               <div className="row mt-1">
                                   <div className="col mt-2 pt-2 text-center">
                                       Don't have account?  <NavLink to="/signup" className="bottom-text">Register now</NavLink>
